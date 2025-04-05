e3cde81a81947071f63161005c2f3fe9c9bf967f
Sebastian Eugenio <82603912+user1228-szp@users.noreply.github.com>
2025-04-05 17:38:49 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,11 +4,8 @@ def crear_interfaz(funcion_iniciar, funcion_detener):
     ventana = tk.Tk()
     ventana.title("ECG")
     ventana.geometry("300x150")
-
     boton_iniciar = tk.Button(ventana, text="Start", command=lambda: [funcion_iniciar(), boton_iniciar.config(state=tk.DISABLED)])
     boton_iniciar.pack(pady=10)
-
     boton_detener = tk.Button(ventana, text="Stop", command=lambda: [funcion_detener(), boton_iniciar.config(state=tk.NORMAL)])
     boton_detener.pack(pady=10)
-
     return ventana, boton_iniciar, boton_detener
